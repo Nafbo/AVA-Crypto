620d9e4d351f625a210132d93253d54e149926aa
Adri2208 <adrian.boyer22@icloud.com>
2022-04-07 19:45:44 +0200

    j'ajoute

--- src/app_test/dash_Adrian/app.py
+++ src/app_test/dash_Adrian/app.py
@@ -8,34 +8,8 @@ dataa=dff
 '''data.sort_values("Balance", inplace=True)'''
 
 app = dash.Dash(__name__)
-'''
-external_stylesheets = [
-    {
-        "href": "/Users/adri22/Desktop/AVA-Crypto/img/icons8-bitcoin-accepted-64.png"
-                "family=Lato:wght@400;700&display=swap",
-        "rel": "stylesheet",
-    },
-]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "Crypto Dashboard : Understand Your Account Easily!"
 
-app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[
-                html.P(children=" /Users/adri22/Desktop/AVA-Crypto/img/icons8-bitcoin-accepted-64.png"),
-                html.H1(
-                    children="Crypto Dashboard : Understand Your Account Easily! ", className="header-title"
-                ),
-                html.P(
-                    children="Get all your wallet and cryptocurrencies prices",
-                    className="header-description",
-                ),
-            ],
-            className="header",
-        ),
 
-'''
 app.layout = html.Div(
     children=[
         html.H1(children="Crypto Dashboard", style={"text-align":"center","fontSize": "48px", "color": "blue"}),
