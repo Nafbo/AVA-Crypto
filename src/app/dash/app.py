a86e4a1a1313e56455e85f109498c9b29d8925de
Al-741 <miermona@gmail.com>
2022-05-28 15:33:45 +0200

    mise en forme register def

--- src/app/dash/app.py
+++ src/app/dash/app.py
@@ -931,11 +931,11 @@ def display_page(pathname):
     
 # ----------------------------- RUN APP ------------------------------------------------ >
 
-# def launch_app():
-#     return app.run_server(debug=False)  *
+def launch_app():
+    return app.run_server(debug=False)  *
 
 
-if __name__=='__main__':
-    app.run_server(debug=True)   
+# if __name__=='__main__':
+#     app.run_server(debug=True)   
 
 
