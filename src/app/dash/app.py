9a34b53f5a59f899497a091da4a6dab7a2565149
Nafbo <victor.bonnaf@gmail.com>
2022-06-02 14:29:51 +0200

    finish

--- src/app/dash/app.py
+++ src/app/dash/app.py
@@ -755,9 +755,9 @@ def update_wallet_id(n_clicks):
         return ''
 # ----------------------------------------------------------------------
 
-@app.callback(Output("modal", "is_open"),[Input("open", "n_clicks"), Input("close", "n_clicks")],[State("modal", "is_open")],)
-def toggle_modal(n1, n2, is_open):
-    if n1 or n2 :
+@app.callback(Output("modal", "is_open"),[Input("open", "n_clicks"), Input("close", "n_clicks"), Input("enter", "n_clicks")],[State("modal", "is_open")],)
+def toggle_modal(n1, n2, n3, is_open):
+    if n1 or n2 or n3 :
         return not is_open
     return is_open
 
